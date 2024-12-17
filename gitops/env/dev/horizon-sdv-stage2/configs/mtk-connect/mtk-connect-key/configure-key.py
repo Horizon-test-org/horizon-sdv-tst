5824f075f24aa9d45dd3efb8d4288aa9e3f99021
marta-accenture <marta.kania@accenture.com>
2024-12-17 14:12:13 +0100

    key expiration date setup

--- gitops/env/dev/horizon-sdv-stage2/configs/mtk-connect/mtk-connect-key/configure-key.py
+++ gitops/env/dev/horizon-sdv-stage2/configs/mtk-connect/mtk-connect-key/configure-key.py
@@ -1,5 +1,7 @@
 import requests
 from enum import Enum
+import datetime
+from dateutil.relativedelta import relativedelta
 
 USERNAME = 
 USER_ID = 
@@ -9,9 +11,12 @@ class API_REQUEST_OPT(Enum) :
   GET_VERSION = "https://dev.horizon-sdv.scpmtk.com/mtk-connect/api/v1/config/version"
   CREATE_KEY = f"https://dev.horizon-sdv.scpmtk.com/mtk-connect/api/v1/users/{USER_ID}/keys"
 
+# New key settings: name, expiration date.
+# Current settings: name is empty, expiration date is set to 1 month after creation date (UTC+00:00 Timezone)
+KEY_EXPIRATION_DATE = datetime.datetime.now(tz=datetime.timezone.utc) + relativedelta(months=1)
 REQUEST_BODY = {
-  "name": "jenkins",
-  "expiryTime": "2024-12-17T11:59:42.367Z",
+  "name": "",
+  "expiryTime": f"{KEY_EXPIRATION_DATE.strftime("%Y-%m-%dT%H:%M:%SZ")}"
 }
 
 def connect_to_api(operation=API_REQUEST_OPT.GET_VERSION, request_body=None):
@@ -20,7 +25,7 @@ def connect_to_api(operation=API_REQUEST_OPT.GET_VERSION, request_body=None):
     if operation is API_REQUEST_OPT.GET_VERSION:
       response_api = requests.get(operation.value, auth=(USERNAME, KEY))
     elif operation is API_REQUEST_OPT.CREATE_KEY:
-      response_api = requests.post(operation.value, auth=(USERNAME, KEY), data=request_body)
+      response_api = requests.post(operation.value, auth=(USERNAME, KEY), json=request_body)
   except Exception as e:
     print(f"Exception occured when requesting response from API. \n\t{e}")
   else:
