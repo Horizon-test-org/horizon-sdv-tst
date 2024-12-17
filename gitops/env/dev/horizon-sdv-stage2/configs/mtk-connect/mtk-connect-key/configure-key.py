1c3820cffdd87f9c3815ac3da4b1e2207869f7ce
marta-accenture <marta.kania@accenture.com>
2024-12-17 12:29:21 +0100

    add exceptions possibility and problems view

--- gitops/env/dev/horizon-sdv-stage2/configs/mtk-connect/mtk-connect-key/configure-key.py
+++ gitops/env/dev/horizon-sdv-stage2/configs/mtk-connect/mtk-connect-key/configure-key.py
@@ -15,19 +15,34 @@ REQUEST_BODY = {
 }
 
 def connect_to_api(operation=API_REQUEST_OPT.GET_VERSION, request_body=None):
-  if operation is API_REQUEST_OPT.GET_VERSION:
-    response_api = requests.get(operation.value, auth=(USERNAME, KEY))
-  elif operation is API_REQUEST_OPT.CREATE_KEY:
-    response_api = requests.post(operation.value, auth=(USERNAME, KEY), data=request_body)
-
-  print(f"status code: {response_api.status_code}")
-  print(f"response:\n{response_api.text}")
-    # return response_api
+  
+  try:
+    if operation is API_REQUEST_OPT.GET_VERSION:
+      response_api = requests.get(operation.value, auth=(USERNAME, KEY))
+    elif operation is API_REQUEST_OPT.CREATE_KEY:
+      response_api = requests.post(operation.value, auth=(USERNAME, KEY), data=request_body)
+  except Exception as e:
+    print(f"Exception occured when requesting response from API. \n\t{e}")
+  else:
+    reponse_category = response_api.status_code // 100
+    if reponse_category == 2:
+      print(f"Operation done succesfully!")
+    elif reponse_category == 3:
+      print(f"Redirection!")
+    elif reponse_category == 4:
+      print(f"Client Error!")
+    elif reponse_category == 5:
+      print(f"Server Error!")
+    else:
+      print(f"There was a problem!")
+  finally:
+    print(f"Status code: {response_api.status_code} \nResponse from API: \n\t{response_api.text}")
+    print("Request to API finished")
 
 
 print("Script start")
-print("Get version of MTK Connect")
+print("\nGet version of MTK Connect")
 connect_to_api()
-print(f"Create key for user id {USER_ID}")
+print(f"\nCreate key for user id {USER_ID}")
 connect_to_api(operation=API_REQUEST_OPT.CREATE_KEY, request_body=REQUEST_BODY)
 
