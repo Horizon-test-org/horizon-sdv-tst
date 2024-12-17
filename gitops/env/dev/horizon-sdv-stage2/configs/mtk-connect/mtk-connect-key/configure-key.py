8286f421aee7e29c1edc5ee924c03f31d411c51e
marta-accenture <marta.kania@accenture.com>
2024-12-17 11:59:19 +0100

    add option to create key for given user

--- gitops/env/dev/horizon-sdv-stage2/configs/mtk-connect/mtk-connect-key/configure-key.py
+++ gitops/env/dev/horizon-sdv-stage2/configs/mtk-connect/mtk-connect-key/configure-key.py
@@ -7,10 +7,18 @@ KEY =
 
 class API_REQUEST_OPT(Enum) :
   GET_VERSION = "https://dev.horizon-sdv.scpmtk.com/mtk-connect/api/v1/config/version"
+  CREATE_KEY = f"https://dev.horizon-sdv.scpmtk.com/mtk-connect/api/v1/users/{USER_ID}/keys"
+
+REQUEST_BODY = {
+  "name": "jenkins",
+  "expiryTime": "2024-12-17T11:59:42.367Z",
+}
 
 def connect_to_api(operation=API_REQUEST_OPT.GET_VERSION, request_body=None):
   if operation is API_REQUEST_OPT.GET_VERSION:
     response_api = requests.get(operation.value, auth=(USERNAME, KEY))
+  elif operation is API_REQUEST_OPT.CREATE_KEY:
+    response_api = requests.post(operation.value, auth=(USERNAME, KEY), data=request_body)
 
   print(f"status code: {response_api.status_code}")
   print(f"response:\n{response_api.text}")
@@ -18,5 +26,8 @@ def connect_to_api(operation=API_REQUEST_OPT.GET_VERSION, request_body=None):
 
 
 print("Script start")
+print("Get version of MTK Connect")
 connect_to_api()
+print(f"Create key for user id {USER_ID}")
+connect_to_api(operation=API_REQUEST_OPT.CREATE_KEY, request_body=REQUEST_BODY)
 
