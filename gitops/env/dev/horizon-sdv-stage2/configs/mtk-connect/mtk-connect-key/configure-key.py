8bf466102eb31ec079d660555d778ab5098f1563
marta-accenture <marta.kania@accenture.com>
2024-12-19 14:52:56 +0100

    move api testing to separate demo function

--- gitops/env/dev/horizon-sdv-stage2/configs/mtk-connect/mtk-connect-key/configure-key.py
+++ gitops/env/dev/horizon-sdv-stage2/configs/mtk-connect/mtk-connect-key/configure-key.py
@@ -60,6 +60,24 @@ def connect_to_api(operation=API_REQUEST_OPT.GET_VERSION, request_body=None, del
     print(f"Status code: {response_api.status_code} \nResponse from API: \n\t{response_api.text}")
     print("Request to API finished")
 
+def demo_api_connection(): # TODO: delete. Just for testing purposes
+  print("\nGet version of MTK Connect")
+  connect_to_api()
+
+  print(f"\nOld key id: {KEY_ID} \tval: {KEY_VAL}")
+
+  print(f"\nCreate key for user id {USER_ID}")
+  connect_to_api(operation=API_REQUEST_OPT.CREATE_KEY, request_body=KEY_CREATE_REQUEST_BODY)
+
+  print(f"\nNew key id: {KEY_ID} \tval: {KEY_VAL}")
+
+  print("\nTesting new key...")
+
+  print("\nGet current user")
+  connect_to_api(operation=API_REQUEST_OPT.GET_CURRENT_USER)
+
+  print(f"\nDeleting key id: {KEY_ID} \tval: {KEY_VAL}")
+  connect_to_api(operation=API_REQUEST_OPT.DELETE_KEY, delete_key_id=KEY_ID)
 
 def create_secret_from_json(json_file):
   with open(json_file, 'r') as f:
@@ -83,23 +101,7 @@ def create_secret_from_json(json_file):
 
 if __name__ == "__main__":
   print("Script start")
-  print("\nGet version of MTK Connect")
-  connect_to_api()
 
-  print(f"\nOld key id: {KEY_ID} \tval: {KEY_VAL}")
-
-  print(f"\nCreate key for user id {USER_ID}")
-  connect_to_api(operation=API_REQUEST_OPT.CREATE_KEY, request_body=KEY_CREATE_REQUEST_BODY)
-
-  print(f"\nNew key id: {KEY_ID} \tval: {KEY_VAL}")
-
-  print("\nTesting new key...")
-
-  print("\nGet current user")
-  connect_to_api(operation=API_REQUEST_OPT.GET_CURRENT_USER)
-
-  print(f"\nDeleting key id: {KEY_ID} \tval: {KEY_VAL}")
-  connect_to_api(operation=API_REQUEST_OPT.DELETE_KEY, delete_key_id=KEY_ID)
   # Load Kubernetes configuration
   config.load_kube_config()
   create_secret_from_json(SECRET_FILE)
