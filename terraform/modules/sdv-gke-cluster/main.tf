8c8262f21583efd589f98f3243c052fc2b080156
lukaszdom <lukasz.domke@accenture.com>
2025-01-08 02:47:19 +0100

    taa 299 jenkins uncontactable 3

--- terraform/modules/sdv-gke-cluster/main.tf
+++ terraform/modules/sdv-gke-cluster/main.tf
@@ -96,6 +96,7 @@ resource "google_container_node_pool" "sdv_build_node_pool" {
   node_count     = var.build_node_pool_node_count
   node_locations = var.node_locations
   node_config {
+    preemptible  = true
     machine_type = var.build_node_pool_machine_type
 
     # Google recommends custom service accounts that have cloud-platform
