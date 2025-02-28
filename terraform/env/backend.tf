0eb140ed075438c3e415742cd20495844093d56a
Wojciech Kobryn <wojciech.kobryn@accenture.com>
2025-02-28 15:54:51 +0100

    TAA-556: remove dependencies on environment variables (#321)
    * TAA-556: remove dependencies on environment variables
    
    * TAA-556: remove dependencies on environment variables - adding cloud region and zone

--- terraform/env/backend.tf
+++ terraform/env/backend.tf
@@ -19,7 +19,6 @@
 terraform {
   required_version = ">= 1.9.6"
   backend "gcs" {
-    bucket = "prj-sbx-horizon-sdv-tf"
-    prefix = "prj-sbx-horizon-sdv-tf-state"
+    prefix = "horizon-sdv-tf-state"
   }
 }
